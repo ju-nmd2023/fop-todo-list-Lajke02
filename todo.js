1a2617ba68b144d22f7e84fedf5dbcc1d6e06fb7
Lykke Karlsson <kaly23qb@student.ju.se>
2024-04-17 23:21:41 +0200

    added javascript & more css

--- todo.js
+++ todo.js
@@ -3,3 +3,44 @@
 // - Mark tasks as done
 // - Delete tasks
 // - Tasks should be stored in Local storage
+
+//importing styles from CSS
+
+//Some constants
+const addTaskButtonElement = document.getElementById("addTask");
+const taskContainerElement = document.getElementById("tasks");
+const inputElement = document.getElementById("input");
+
+// clicking the button creates a task
+addTaskButtonElement.addEventListener("click", () => {
+  if (inputElement.value.length > 0) {
+    createTask();
+  }
+});
+
+function createTask() {
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("taskDiv");
+  taskContainerElement.appendChild(taskElement);
+
+  const taskTextElement = document.createElement("p");
+  taskTextElement.classList.add("task-element");
+  taskTextElement.innerText = inputElement.value;
+  taskElement.appendChild(taskTextElement);
+
+  inputElement.value = "";
+
+  const checkedOffButtonElement = document.createElement("button");
+  checkedOffButtonElement.classList.add("checked-button");
+  checkedOffButtonElement.innerText = "✔️";
+  taskElement.appendChild(checkedOffButtonElement);
+
+  const deleteButtonElement = document.createElement("button");
+  deleteButtonElement.classList.add("delete-button");
+  deleteButtonElement.innerText = "✖️";
+  taskElement.appendChild(deleteButtonElement);
+
+  deleteButtonElement.addEventListener("click", () => {
+    taskContainerElement.removeChild(taskElement);
+  });
+}
