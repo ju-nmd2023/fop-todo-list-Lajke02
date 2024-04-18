a0289b65881f9274e37495ecaa6484338d31e0d3
Lykke Karlsson <kaly23qb@student.ju.se>
2024-04-18 19:04:16 +0200

    made the checked off button work
    now it turns green yay

--- todo.js
+++ todo.js
@@ -1,23 +1,24 @@
 // be able to:
-// - add tasks
-// - Mark tasks as done
-// - Delete tasks
+// - add tasks ✔️
+// - Mark tasks as done ✔️
+// - Delete tasks ✔️
 // - Tasks should be stored in Local storage
 
 //importing styles from CSS
 
-//Some constants
+//Some constants needed before creating a task
 const addTaskButtonElement = document.getElementById("addTask");
 const taskContainerElement = document.getElementById("tasks");
 const inputElement = document.getElementById("input");
 
-// clicking the button creates a task
+// clicking the button creates a task (add task)
 addTaskButtonElement.addEventListener("click", () => {
   if (inputElement.value.length > 0) {
     createTask();
   }
 });
 
+//Creating a task and what it intails
 function createTask() {
   const taskElement = document.createElement("div");
   taskElement.classList.add("taskDiv");
@@ -28,18 +29,30 @@ function createTask() {
   taskTextElement.innerText = inputElement.value;
   taskElement.appendChild(taskTextElement);
 
+  // clearing the textinput after writing a task
   inputElement.value = "";
 
+  // the checked off button
   const checkedOffButtonElement = document.createElement("button");
   checkedOffButtonElement.classList.add("checked-button");
   checkedOffButtonElement.innerText = "✔️";
   taskElement.appendChild(checkedOffButtonElement);
 
+  // makes the task field green to signify that it's done (mark as done)
+  checkedOffButtonElement.addEventListener("click", () => {
+    if (taskElement.style.backgroundColor !== "lightgreen") {
+      taskElement.style.backgroundColor = "lightgreen";
+    } else if (taskElement.style.backgroundColor !== "white") {
+      taskElement.style.backgroundColor = "white";
+    }
+  });
+  // delete button
   const deleteButtonElement = document.createElement("button");
   deleteButtonElement.classList.add("delete-button");
   deleteButtonElement.innerText = "✖️";
   taskElement.appendChild(deleteButtonElement);
 
+  //makes task disappear into the void (delete task)
   deleteButtonElement.addEventListener("click", () => {
     taskContainerElement.removeChild(taskElement);
   });
